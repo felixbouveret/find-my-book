51a0c42ce78f49047c02c60bc57e34eb6dec595b
Félix Bouveret <bouveret.felix@gmail.com>
2021-03-13 17:15:29 +0100

    Add gobal Wrapper

---
+++ components/Wrapper/Wrapper.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.inner">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Wrapper',
+};
+</script>
+
+<style lang="scss" module>
+.inner {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
