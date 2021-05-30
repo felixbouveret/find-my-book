f702c9a5d71c351cc8f68e7843e7a41004da146a
Félix Bouveret <bouveret.felix@gmail.com>
2021-05-30 10:55:44 +0200

    feat(header): add username and disconnect button

--- modules/Navbar/components/LogLinks/LogLinks.vue
+++ modules/Navbar/components/LogLinks/LogLinks.vue
@@ -1,20 +1,36 @@
 <template>
-  <ul :class="$style.list">
-    <li><a href=""></a>Sign in</li>
+  <ul v-if="!connected" :class="$style.list">
+    <li><a href="/auth?login=true">Sign in</a></li>
     <li>
       <Button>
-        <a href="">Sign up</a>
+        <a href="/auth">Sign up</a>
       </Button>
     </li>
   </ul>
+  <div v-else :class="$style.list">
+    <p>
+      {{ username }}
+    </p>
+    <button @click="disconnectUser">Disconnect</button>
+  </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
 import Button from '~/components/Button';
 
 export default {
   name: 'LogLinks',
+
   components: { Button },
+
+  computed: {
+    ...mapState('user', ['connected', 'username']),
+  },
+
+  methods: {
+    ...mapActions('user', ['disconnectUser']),
+  },
 };
 </script>
 
