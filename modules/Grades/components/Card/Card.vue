2c27bd03d8a1a6e237a6f9ae0484c1f97f6d558d
teyz <contact@bastienrigaud.fr>
2021-06-07 23:54:09 +0200

    Features : Add grades page

---
+++ modules/Grades/components/Card/Card.vue
@@ -0,0 +1,71 @@
+<template>
+  <div :class="$style.cardRoot">
+    <img :src="cardData.book.img_url" alt="" />
+    <div :class="$style.cardDetails">
+      <h2 :class="$style.singleTitle">{{ cardData.book.name }}</h2>
+      <p :class="$style.singleAuthor">by {{ cardData.book.auteur }}</p>
+      <p :class="$style.singleAverage">Average : {{ cardData.average }}</p>
+      <p :class="$style.singleSynopsys">{{ cardData.book.synopsis }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.cardRoot {
+  position: relative;
+
+  display: flex;
+
+  width: 500px;
+  height: 300px;
+  padding: 24px;
+  border-radius: 5px;
+
+  background-color: #f3f3f3;
+
+  &:before {
+    position: absolute;
+    top: -50px;
+    left: -45px;
+
+    padding: 24px;
+
+    border-radius: 1000px;
+
+    background-color: #ff007a;
+    transform: scale(0.6);
+
+    content: url('~/assets/img/book.svg');
+  }
+
+  .cardDetails {
+    margin-left: 24px;
+    .singleTitle {
+      font-size: 14px;
+    }
+    .singleAuthor,
+    .singleSynopsys,
+    .singleAverage {
+      margin-top: 10px;
+    }
+    .singleSynopsys {
+      display: -webkit-box;
+      max-height: 200px;
+      overflow: hidden;
+      -webkit-line-clamp: 10;
+      -webkit-box-orient: vertical;
+    }
+  }
+}
+</style>
---
+++ pages/grades.vue
@@ -0,0 +1,21 @@
+<template>
+  <Wrapper>
+    <GradesModule :module-data="data" />
+  </Wrapper>
+</template>
+
+<script>
+import GradesModule from '~/modules/Grades';
+export default {
+  components: { GradesModule },
+  middleware: 'auth/isNotConnected',
+
+  async asyncData() {
+    const grades = await fetch(`http://localhost:8000/books/average/10`)
+      .then((res) => res.json())
+      .catch((err) => console.error(err));
+
+    return { data: grades };
+  },
+};
+</script>
