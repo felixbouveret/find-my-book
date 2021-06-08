79d238e7f8301e595419c0813e08041e71730f5d
Estelle-dng <estelle.denage@lapiscine.pro>
2021-06-09 00:36:41 +0200

    add account page and link

---
+++ modules/Account/Account.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="$style.container">
+    <p>Username : {{ username }}</p>
+    <p>Email : {{ email }}</p>
+    <Button @click="disconnectUser"> Deconnexion </Button>
+    <a href="#">Delete account</a>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import Button from '~/components/Button';
+
+export default {
+  name: 'AccountModule',
+
+  components: { Button },
+
+  computed: {
+    ...mapState('user', ['connected', 'username']),
+  },
+
+  methods: {
+    ...mapActions('user', ['disconnectUser']),
+  },
+};
+</script>
+
+<style lang="scss" module>
+.container {
+  height: calc(100vh - 92px);
+  margin-top: 20px;
+}
+</style>
---
+++ pages/account.vue
@@ -0,0 +1,22 @@
+<template>
+  <Account :module-data="data" />
+</template>
+
+<script>
+import Account from '~/modules/Account';
+
+export default {
+  components: { Account },
+  middleware: 'auth/isNotConnected',
+
+  async asyncData({ $axios }) {
+    try {
+      const data = await $axios.$get(`account/all`);
+
+      return { data };
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
