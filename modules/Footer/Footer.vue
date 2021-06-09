cc5eb7d48e9d12875c2c06198f8be9c985734d8d
teyz <contact@bastienrigaud.fr>
2021-06-09 22:30:16 +0200

    Features : WIP design

---
+++ modules/Footer/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="$style.footer">
+    <Wrapper :class="$style.inner">
+      <a href="/">
+        <img
+          :class="$style.logo"
+          src="~/assets/img/findmybooklogo.svg"
+          alt=""
+        />
+      </a>
+      <a href="">test</a>
+      <a href="">test</a>
+      <p>Copyright : Benoit, Bastien, Félix, Estelle</p>
+    </Wrapper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterModule',
+};
+</script>
+
+<style lang="scss" module>
+.footer {
+  padding: 24px;
+
+  background-color: $light-grey;
+}
+
+.inner {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  height: 60px;
+}
+</style>
