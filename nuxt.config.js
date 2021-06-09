655e7be404a4635a3bc1b3efc3429b67b2f45d85
teyz <contact@bastienrigaud.fr>
2021-06-09 17:40:46 +0200

    Features : Add global scss variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,14 +12,28 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  css: ['@/styles/reset.css', '@/styles/commons'],
+  css: ['@/styles/reset.css', '@/styles/commons', '@/styles/_modules'],
 
   plugins: ['~/plugins/globalComponents.js', '~/plugins/axios'],
 
   components: true,
 
-  modules: ['@nuxtjs/axios', 'cookie-universal-nuxt', '@nuxtjs/toast'],
-  buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module', '@nuxtjs/svg'],
+  modules: [
+    '@nuxtjs/axios',
+    'cookie-universal-nuxt',
+    '@nuxtjs/toast',
+    '@nuxtjs/style-resources',
+  ],
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/svg',
+  ],
+
+  styleResources: {
+    scss: ['@/styles/*.scss'],
+  },
 
   axios: {
     baseURL: process.env.API_ENTRY_POINT,
